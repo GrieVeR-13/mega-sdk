869e04e219215cd9c8a953f3b4febf969d552799
GrieVeR-13 <griever-c@list.ru>
2025-12-03 13:19:32 +0300

    ares fix

--- examples/android/ExampleApp/app/src/main/jni/curl/Android.mk
+++ examples/android/ExampleApp/app/src/main/jni/curl/Android.mk
@@ -10,18 +10,18 @@ LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/curl/curl-android-$(TARGET_ARCH_ABI)/in
 
 LOCAL_LDFLAGS += "-Wl,-z,max-page-size=16384"
 
-LOCAL_STATIC_LIBRARIES := ares ssl crypto
+LOCAL_STATIC_LIBRARIES := ssl crypto
 
 
-include $(PREBUILT_STATIC_LIBRARY)
+# include $(PREBUILT_STATIC_LIBRARY)
 
 
-include $(CLEAR_VARS)
+# include $(CLEAR_VARS)
 
-LOCAL_MODULE := ares
-LOCAL_SRC_FILES := $(LOCAL_PATH)/ares/ares-android-$(TARGET_ARCH_ABI)/lib/libcares.a
-
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/ares/ares-android-$(TARGET_ARCH_ABI)/include
+# LOCAL_MODULE := ares
+# LOCAL_SRC_FILES := $(LOCAL_PATH)/ares/ares-android-$(TARGET_ARCH_ABI)/lib/libcares.a
+#
+# LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/ares/ares-android-$(TARGET_ARCH_ABI)/include
 
 
 LOCAL_LDFLAGS += "-Wl,-z,max-page-size=16384"
